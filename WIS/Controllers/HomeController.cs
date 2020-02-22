804c06776bd92fb7deb7c60930eb545fce3e4abb
Qifeng <qifeng.fang@qq.com>
2020-02-22 18:36:37 +1300

    添加项目文件。

---
+++ WIS/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WIS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //Main form page
+        public ActionResult Index()
+        {
+            ViewBag.welcomeInfo = "Welcome Institute of Studies";
+            return View();
+        }
+
+        //This a register page
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        //Login Page.
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
