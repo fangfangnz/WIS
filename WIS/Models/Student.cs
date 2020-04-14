99d12ca8bdee0abc05ece113d19c15738ed04e9c
Qifeng <qifeng.fang@qq.com>
2020-04-14 17:18:22 +1200

    after review

---
+++ WIS/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WIS.Models
+{
+    public class Student
+    {
+
+        public int id { get; set; }
+
+
+        public string stuName { get; set; }
+    }
+}
