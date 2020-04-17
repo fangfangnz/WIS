60889471133f8bfb92bc891e1b88c94f67f3d16e
Qifeng <qifeng.fang@qq.com>
2020-04-17 15:47:36 +1200

    second commit

---
+++ WIS/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WIS.Models
+{
+    //student class
+    public class Student 
+    {
+
+        public int id { get; set; } 
+
+
+        public string stuName { get; set; } 
+
+        public GENDER gender { get; set; }
+
+    }
+
+
+    //gender enum
+    public enum GENDER
+    {
+        M=0,
+        F=1
+    }
+
+
+  
+}
---
+++ WIS/Models/WisStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WIS.Models
+{
+    //WIS student entity
+    public class WisStudent : IStudent
+    {
+        public string sayHello(string name, GENDER gender) 
+        {
+
+            return $"I am {gender},My name is {name}";
+        }
+       
+    }
+}
